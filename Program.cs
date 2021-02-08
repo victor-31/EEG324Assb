a75660e48def114efd3b366e19d0d684902fd5ca
victor-31 <78726483+victor-31@users.noreply.github.com>
2021-02-08 04:48:55 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using system.Collection.Generic;
+using System.Linq;
+using System.Text;
+;
+using System.Threading.Tasks;
+
+class miles driven;
+{
+    public double miles = "30 miles";
+{
+    Console.WriteLine("30miles");
+}
+}
+class gallon used;
+{
+    public double gallon = "10 gallons";
+{
+    Console.WriteLine("10 gallons");
+}
+}
+{
+public class program
+    static void Main(string[] args)
+{
+    miles per gallon = (miles driven / gallon used);
+Console.WriteLine("30miles / 10 gallons");
+Console.WriteLine("miles per gallon = 3miles");
+}
+
+}
